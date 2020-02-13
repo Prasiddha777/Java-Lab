ee69b113b72940c14a325a0a8b334d661f423d2c
Prasiddha777 <be2018se630@gces.edu.np>
2020-02-14 05:12:44 +0545

    changes

---
+++ Lab01/lab01multi.java
@@ -0,0 +1,19 @@
+//MultiDimensional Array
+
+class Testarray{  
+public static void main(String args[]){  
+//declaring and initializing 2D array  
+int arr[][]={{1,2,3},{2,4,5},{4,4,5}};
+for(int i=0;i<3;i++){  
+ for(int j=0;j<3;j++){  
+   System.out.print(arr[i][j]+" ");  
+ }  
+ System.out.println();  
+}  
+}}  
+```
+
+Output
+1 2 3\
+2 4 5\
+4 4 5
